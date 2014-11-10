78af2413c2c45e2cc006724be657560a8925682d
yurivm <yuri.veremeyenko@gmail.com>
2014-11-10 22:50:30 +0100

    add the scripts to generate input CSV and process it

--- bin/calc_jaccard_similarities.rb
+++ bin/calc_jaccard_similarities.rb
@@ -1,20 +1,42 @@
 #!/usr/bin/env ruby
 require 'dumagst'
+require 'logger'
+
+logger = Logger.new(STDOUT)
+logger.level = Logger::DEBUG
 
 Dumagst.configure do |config|
   config.host = "localhost"
   config.port = 6379
   config.db = 1
 end
-filename = File.join(
+# filename = File.join(
+#   File.dirname(__FILE__),
+#   "..",
+#   "spec",
+#   "fixtures",
+#   "products_users_sorted_1656_943.csv"
+# )
+data_dir = File.join(
   File.dirname(__FILE__),
   "..",
-  "spec",
-  "fixtures",
-  "products_users_sorted_1656_943.csv"
+  "data"
 )
+input_filename = File.join(data_dir, "data.csv")
+
+dimensions_filename = File.join(data_dir, "dimensions.csv")
+
+dimensions = []
+CSV.foreach(dimensions_filename, "r") do |row|
+  dimensions = [row[0].to_i, row[1].to_i]
+end
+logger.info "Determining similar products: Jaccard Engine"
+logger.info "Input file : #{File.absolute_path(input_filename)}"
+logger.info "Matrix dimensions: #{dimensions[0]} products by #{dimensions[1]} users"
 Dumagst.configuration.redis_connection.flushdb
-matrix = Dumagst::Matrices::NativeMatrix.from_csv(filename, 1656, 943)
+matrix = Dumagst::Matrices::NativeMatrix.from_csv(input_filename, dimensions[0], dimensions[1])
 engine = Dumagst::Engines::JaccardEngine.new(engine_key: "jaccard_native_similarity", matrix: matrix, similarity_threshold: 0.5)
 
 engine.process
+
+logger.info "Done."
--- bin/prepare_csv.rb
+++ bin/prepare_csv.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/env ruby
 require 'csv'
 require 'yaml'
+require 'logger'
+
+logger = Logger.new(STDOUT)
+logger.level = Logger::DEBUG
+
 dirname = File.dirname(__FILE__)
-input_filename = ARGV[0] || File.join(dirname, "..", "ml-100k", "data", "u.data")
+input_filename = ARGV[0] || File.join(dirname, "..", "data", "ml-100k", "u.data")
 output_filename = ARGV[1] || File.join(dirname, "..", "data", "data.csv")
+metadata_filename = File.join(File.dirname(output_filename), "dimensions.csv")
 #fixture_filename = File.join(dirname, "..", "spec", "fixtures", "products_users.csv")
-threshold = (ARGV[2] || 4).to_i
+threshold = (ARGV[2] || 3).to_i
 
 output_file = CSV.open(output_filename, "wb")
 #fixtures_file = CSV.open(fixture_filename, "wb")
@@ -16,10 +22,15 @@ users = []
 products = []
 products_per_user = {}
 
+logger.info "processing movie ratings:"
+logger.debug "input file : #{File.absolute_path(input_filename)}"
+logger.debug "output file : #{File.absolute_path(output_filename)}"
+logger.debug "minimal rating to be considered a like : #{threshold}"
+
 CSV.foreach(input_filename, col_sep: "\t") do |row|
   total_count += 1
   #user id | item id | rating | timestamp.
-  if row[2].to_i > threshold
+  if row[2].to_i >= threshold
     # save as item_id | user_id
     output_file << [row[1], row[0]]
     liked_count +=1
@@ -32,15 +43,23 @@ CSV.foreach(input_filename, col_sep: "\t") do |row|
     #   fixtures_file << [row[1], row[0]]
     #   fixtures_count += 1
     # end
+    logger.info "Processed #{total_count} records " if total_count % 10000 == 0
   end
 end
 
 output_file.close
 #fixtures_file.close
+max_user = users.max_by {|id| id.to_i}
+max_product = products.max_by {|id| id.to_i}
+
+CSV.open(metadata_filename, "wb") do |csv|
+  csv << [max_product, max_user]
+end
 
 users.uniq!
 products.uniq!
 avg_liked = products_per_user.values.inject{ |sum, el| sum + el }.to_f / products_per_user.values.size
 
-puts "Processed #{total_count} records, produced #{liked_count} user like records. Total users #{users.count}, total products #{products.count}, average of #{avg_liked} per user"
+logger.info "Processed #{total_count} records, produced #{liked_count} user like records. Total users #{users.count}, total products #{products.count}, average of #{avg_liked} per user"
+logger.info "Matrix dimensions : #{max_product} products by #{max_user} users"
 #puts "Saved #{fixtures_count} records to the fixture file #{File.absolute_path(fixture_filename)}"
